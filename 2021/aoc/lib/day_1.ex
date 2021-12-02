6616abbda596b765c598c21a55347767e35bb639
Malian De Ron <deronmalian@gmail.com>
2021-12-02 03:18:29 +0100

    Ad convenience functions

--- 2021/aoc/lib/day_1.ex
+++ 2021/aoc/lib/day_1.ex
@@ -8,4 +8,19 @@ defmodule AOC.Day1 do
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.count(fn [a, b] -> b > a end)
   end
+
+  # Convenience functions for AOC
+  @inputs AOC.parse_file("../inputs/01/inputs.txt")
+
+  def part1 do
+    @inputs
+    |> count_measurements_that_are_larger_than_the_previous_one()
+    |> IO.inspect(label: "Answer")
+  end
+
+  def part2 do
+    @inputs
+    |> count_measurements_that_are_larger_than_the_previous_one(chunk: 3)
+    |> IO.inspect(label: "Answer")
+  end
 end
